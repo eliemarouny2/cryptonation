fa8b883a749beed8e2aebd6a288e562a2a310784
ghidam <ghida_dm@hotmail.co.uk>
2022-01-20 22:47:14 +0200

    Implementation of blogs gallery

--- cryptonation/resources/views/blogs.blade.php
+++ cryptonation/resources/views/blogs.blade.php
@@ -2,6 +2,10 @@
 @section('content')
 <section class="blogs">
 			<div class="imageblog">
+				<div class="welcome-title">
+					<p class="welcome-to"> Welcome to our </p>
+					<p class="blogs-title"> Blogs </p>
+				</div>
 				<div class="description">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nam,
 					rerum, ex unde qui eligendi quod quisquam cumque perferendis, voluptas
@@ -10,10 +14,34 @@
 					Laborum, veniam.
 				</div>
 			</div>
-		</section>
-        <section class="blogs2 mb-4">
-			<div class="centered">
-				<a class="readmore" href="">read more</a>
-			</div>
-		</section>
+</section>
+<section class="blogs2 mb-4">
+	<div class="centered">
+			<a class="readmore" href="">read more</a>
+	</div>
+</section>
+<section class="container album">
+	<div class="row1">
+		<div class="div1-1 album-border">
+			<img class="img1-1" src="images/vlogs/1641127816.png">
+		</div>
+		<div class="div1-2 album-border">
+			<img class="img1-2" src="images/vlogs/1641127816.png">
+		</div><div class="div1-3 album-border">
+			<img class="img1-3" src="images/vlogs/1641127816.png">
+		</div>
+	</div>
+	<div class="row2">
+	<div class="div2-1 album-border">
+			<img class="img2-1" src="images/vlogs/1641127816.png">
+		</div>
+		<div class="div2-1 album-border">
+			<img class="img2-1" src="images/vlogs/1641127816.png">
+		</div>
+		<div class="div2-3 album-border">
+			<img class="img2-3" src="images/vlogs/1641127816.png">
+		</div>
+	</div>
+</section>
+
 @endsection
