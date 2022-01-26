7794e1cee2c3f7a46cb2779bf27e779e27463b3c
Elie Marouny <EMarouny@areeba.com>
2022-01-26 11:13:45 +0200

    login and registration responsive

--- cryptonation/resources/views/auth/login.blade.php
+++ cryptonation/resources/views/auth/login.blade.php
@@ -8,6 +8,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/styles.css">
+        <link rel="stylesheet" href="/css/responsive.css">
         <title>Sign in</title>
     </head>
 
@@ -16,12 +17,11 @@
 
         <div class="row g-5 mt-3 mb-3">
             <div class="col-md-4 col-lg-4">
-
                 <img src="/images/icons/astro.png" class="img-astro" alt="astraunot image">
             </div>
             <div class="col-md-2 col-lg-2">
             </div>
-            <div class="col-md-6 col-lg-6 margin-top-large">
+            <div class="col-md-6 col-lg-6 margin-top-large padding-for-sm">
                 <h1 class="mb-3 greenish mt-5">Welcome back</h1>
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
@@ -34,13 +34,13 @@
                         </div>
                     </div>
                     <div class="row g-3">
-                        <div class="col-sm-5">
+                        <div class="col-sm-12 col-lg-6 col-md-6">
                             <input type="email" class="form-control" id="email" name="email" placeholder="Email"
                                 :value="old('email')" required>
                         </div>
                     </div>
                     <div class="row g-3 mt-2">
-                        <div class="col-sm-5">
+                        <div class="col-sm-12 col-lg-6 col-md-6">
                             <input type="password" class="form-control" id="password" name="password"
                                 placeholder="Password" required autocomplete="current-password">
                         </div>
--- cryptonation/resources/views/auth/register.blade.php
+++ cryptonation/resources/views/auth/register.blade.php
@@ -8,19 +8,20 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/styles.css">
+        <link rel="stylesheet" href="/css/responsive.css">
         <title>Sign Up</title>
     </head>
 
     <body>
 
-        <div class="row g-5 mt-3 mb-3">
+        <div class="row g-5 mb-3">
             <div class="col-md-4 col-lg-4">
 
                 <img src="/images/icons/astro.png" class="img-astro" alt="astraunot image">
             </div>
             <div class="col-md-1 col-lg-1">
             </div>
-            <div class="col-md-6 col-lg-6 margin-top-large">
+            <div class="col-md-6 col-lg-6 margin-top-large padding-for-sm">
                 <h1 class="mb-3 bluish2 mt-5 mb-5">Get started</h1>
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
@@ -33,17 +34,17 @@
                         </div>
                     </div>
                     <div class="row g-3">
-                        <div class="col-sm-4">
+                        <div class="col-md-6 col-lg-4">
                             <input type="text" class="form-control" id="name" name="name" placeholder="Full name"
                                 required autofocus>
                         </div>
-                        <div class="col-sm-4">
+                        <div class="col-md-6 col-lg-4">
                             <input type="text" class="form-control" id="username" name="username" placeholder="Username"
                                 required>
                         </div>
                     </div>
                     <div class="row g-3 mt-1">
-                        <div class="col-sm-4">
+                        <div class="col-md-6 col-lg-4">
                             <div class="input-group">
                                 <select name="phonecode" id="phonecode" class="greenish phonecode">
                                     <option value="+961">+961</option>
@@ -51,28 +52,28 @@
                                 <input type="number" name="number" id="number" class="form-control" required>
                             </div>
                         </div>
-                        <div class="col-sm-4">
+                        <div class="col-md-6 col-lg-4">
                             <input type="email" class="form-control" id="email" name="email" placeholder="Email"
                                 required>
                         </div>
                     </div>
                     <div class="row g-3 mt-1">
-                        <div class="col-sm-4">
+                        <div class="col-md-6 col-lg-4">
                             <input type="date" class="form-control greenish" id="date" name="date" required>
                         </div>
-                        <div class="col-sm-4">
+                        <div class="col-md-6 col-lg-4">
                             <input type="password" class="form-control" id="password" name="password"
                                 placeholder="Password" required>
                         </div>
                     </div>
                     <div class="row g-3 mt-1">
-                        <div class="col-sm-4">
+                        <div class="col-md-6 col-lg-4">
                             <input type="password" class="form-control" id="password_confirmation"
                                 name="password_confirmation" placeholder="Confirm password" required>
                         </div>
                     </div>
                     <div class="row g-3 mt-1">
-                        <div class="col-sm-6">
+                        <div class="col-md-6 col-lg-4">
                             <input class="form-check-input" type="checkbox" value="terms" id="terms" required>
                             <label class="form-check-label" for="flexCheckDefault">
                                 I accept all terms and conditions
